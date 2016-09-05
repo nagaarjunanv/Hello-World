5d1f832aa5827b4f6594491ecdba1fc6361899c3
nagaarjunanv <14manikandan1996@gmail.com>
2016-09-05 07:11:17 -0700

    Create HelloWolrd.java

---
+++ HelloWolrd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner value=new Scanner(System.in);
+	 float avalue=value.nextFloat();
+	 if(avalue>=0)
+	 System.out.println("the given number is positive");
+	 else
+	 System.out.println("the number is negative");
+	}
+}
